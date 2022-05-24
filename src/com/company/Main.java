0b384df231059e52bcf17b84333278c8a3396839
flora-code <floraikanga112@gmail.com>
2022-05-24 15:23:09 +0300

    LAB 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,12 @@ public class Main {
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
+        finalPosition = finalPositionCalc(gravity, 10, 0, 0);
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
+    }
+
+    public static double finalPositionCalc(double acceleration, int time, double initialVelocity, double pos) {
+        return (0.5 * acceleration * time * time) + (initialVelocity * time) + pos;
     }
 }
